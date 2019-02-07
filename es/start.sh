13789367acc2394374919a887ba086ff4d2cf180
guriguri <guriguri.kr@gmail.com>
2019-02-07 23:17:58 +0900

    저사양 장비에서 supervisord 로 elasticsearch, kibana 실행 안되고 죽는 현상이 있어서 daemon 으로 실행하도록 수정

--- es/start.sh
+++ es/start.sh
@@ -4,4 +4,10 @@ set -x
 # start supervisord
 /usr/bin/supervisord &
 
+# start elasticsearch, kibana by daemon
+service elasticsearch start
+service kibana start
+/etc/init.d/daemon elasticsearch &
+/etc/init.d/daemon kibana &
+
 tail -f `find /var/log -name *.log -or -name *.out`
